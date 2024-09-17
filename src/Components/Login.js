a1e7bce0cd265556b371dbf2ac349583b68d1efe
dhanshreepande <dmpande92@gmail.com>
2024-09-17 19:53:26 +0530

    sign in form done

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Header from './Header'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <Header />
+        <div className='absolute'> 
+         <img className=""src ="https://assets.nflxext.com/ffe/siteui/vlv3/85ff76db-39e5-423a-afbc-97d3e74db71b/null/IN-en-20240909-TRIFECTA-perspective_b22117e0-4610-4d57-a695-20f77d241a4a_large.jpg"  alt="backgroundimage" />    
+        </div>
+        <form className="text-white absolute w-3/12  p-12 bg-black my-36 mx-auto right-0 left-0 bg-opacity-80">
+        <h1 className='font-bold  text-3xl py-4'>Sign In</h1>
+          <input className="p-2 my-4 w-full bg-gray-700 rounded-sm"type = "text" placeholder ="Email Address"></input>
+          <input className="p-2 my-4 w-full bg-gray-700 rounded-sm"type = "password" placeholder ="Password"></input>
+          <button className='w-full p-2 my-6  bg-red-700 rounded-lg'>Sign In</button>
+        </form>
+    </div>
   )
 }
 
